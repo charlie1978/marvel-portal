7861b50546d61ca5465d15099bc57ed6c55d474e
Diani Aguirre <dianiyajanciflorez123@gmail.com>
2022-12-23 19:47:01 -0500

    Se pusò a funcionar el nuevo componente para odernar los nombres de Z-A

--- src/modules/marvel-characters/pages/CharactersPage/index.jsx
+++ src/modules/marvel-characters/pages/CharactersPage/index.jsx
@@ -3,8 +3,17 @@ import CharactersGrid from '@/modules/marvel-characters/components/CharacterGrid
 import FeaturedCharacters from '../../components/FeaturedCharacteres';
 import './styles.scss';
 import CharacterProm from '@/modules/core/components/molecules/CharacterProm';
+import { useState } from 'react';
 
 export function CharactersPage() {
+  const [select, setselect] = useState('A-Z');
+  function Characteroder() {
+    if (select == 'A-Z') {
+      return <CharactersGrid n={1} />;
+    } else {
+      return <CharactersGrid n={2} />;
+    }
+  }
   return (
     <>
       <div className="mvl-characters-page">
@@ -19,9 +28,27 @@ export function CharactersPage() {
           <h1 className="featured-text">FEATURED CHARACTERS</h1>
           <FeaturedCharacters />
           <h1 className="title-character">MARVEL CHARACTERS LIST</h1>
-          <CharactersGrid />
+          <div>
+            <h3>Ordenar por</h3>
+          </div>
+          <div>
+            <select
+              className="Arizona"
+              id="lang"
+              value={select}
+              onChange={(e) => setselect(e.target.value)}>
+              <option value="A-Z" tabIndex="0" className="Option-order-active " type="checkbox">
+                A-Z
+              </option>
+              <option value="Z-A" tabIndex="0" className="Option-order" type="checkbox">
+                Z-A
+              </option>
+            </select>
+          </div>
         </div>
       </div>
+
+      <Characteroder />
       <CharacterProm />
     </>
   );
