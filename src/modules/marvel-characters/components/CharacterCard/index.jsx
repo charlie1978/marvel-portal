8d4dc046b94f3c6519cc5f8d1dbe54b9e85e7530
charlie1978 <charlie.net@gmail.com>
2022-12-29 02:01:39 -0500

    carlitos reordenando parte xx01

--- src/modules/marvel-characters/components/CharacterCard/index.jsx
+++ src/modules/marvel-characters/components/CharacterCard/index.jsx
@@ -11,21 +11,17 @@ CharacterCard.propTypes = {
 };
 
 export default function CharacterCard({ image, name, description, isSkeleton }) {
-  const currentDate = new Date().toDateString();
   return (
     <div className="mvl-character-card" data-is-skeleton={isSkeleton}>
       <div className="mvl-character-card__image-container">
         <img className="mvl-character-card__image" src={image} alt="" />
-      </div>
-      <div className="mvl-character-card__body-container">
-        <div className="mvl-character-card__body">
-          <h3 className="u-no-margin">{name}</h3>
-          <h5 className="marvel-label">1939 Timely Comics</h5>
-          <h6 className="marvel-label"> </h6>
-          <h3 className="u-no-margin"> </h3>
-          <h3 className="u-no-margin">{description}</h3>
-          <h6 className="marvel-label">{currentDate}</h6>
-          <h4 className="u-no-margin">{name}</h4>
+        <div className="mvl-character-card__body-container">
+          <div className="mvl-character-card__body">
+            <h3 className="u-no-margin">{name}</h3>
+            <h5 className="marvel-label">1939 Timely Comics</h5>
+            <h3 className="u-no-margin">{description}</h3>
+            <h4 className="u-no-margin">{name}</h4>
+          </div>
         </div>
       </div>
     </div>
